5e626b21bf0a7cb0bc1445c353dd24a55cbed564
munroe1786 <era3munroe@gmail.com>
2020-03-26 16:19:55 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+let divide = function() {
+    return (2000 / 100)
+}
+
+let square = numberToBeSquared => numberToBeSquared * numberToBeSquared;
+
+let add = (num1, num2) => {
+    return num1 + num2;
+}
